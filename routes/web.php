b298f08de3aeb3e267b4e885323763e0c8e3b265
jimenaC <jccp2323@hotmail.com>
2026-01-28 08:26:46 -0500

    Implementación de impresion de reporte en PDF

--- routes/web.php
+++ routes/web.php
@@ -3,19 +3,26 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PersonasController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
 
 //Route::get('/personas/pendientes', [PersonasController::class, 'obtenerPersonas']);
 //Route::get('/registrar-persona', [PersonasController::class, 'registrarPersona'])->name('personas.registrar');
-Route::get('/personas/crear', [PersonasController::class, 'create'])->name('personas.crear');
-Route::post('/personas/store', [PersonasController::class, 'store'])->name('personas.store');
+Route::get('/personas/crear', [PersonasController::class, 'create'])
+    ->name('personas.crear');
+Route::post('/personas/store', [PersonasController::class, 'store'])
+    ->name('personas.store');
 
 Route::get('/personas', [PersonasController::class, 'index'])->name('personas.index');
-Route::get('/personas/{id}/reporte', [PersonasController::class, 'reporte'])->name('personas.reporte');
+Route::get('/personas/{id}/reporte', [PersonasController::class, 'reporte'])
+    ->name('personas.reporte');
 
-Route::post('/personas/{persona}/retry', [PersonasController::class, 'retry'])->name('personas.retry');
+Route::post('/personas/{persona}/retry', [PersonasController::class, 'retry'])
+    ->name('personas.retry');
+
+Route::get('/personas/{ppersonadoc}/reporte/pdf', [PersonasController::class, 'reportePdf'])
+    ->name('personas.reporte.pdf');
+
+
+
 
 
 
